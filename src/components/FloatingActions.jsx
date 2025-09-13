87dc245f87b22695816797068349433e41398e87
maliandri <maliandri@outlook.com>
2025-09-13 11:11:07 -0300

    nueva secton

--- src/components/FloatingActions.jsx
+++ src/components/FloatingActions.jsx
@@ -5,12 +5,10 @@ import DashboardStatsV2 from "./DashboardStats";
 import CVATSUploader from "./CVATSUploader";
 import ROICalculator from "./Calculadora";
 import WebCalculator from "./CalculadoraWeb";
-import BCRADashboardOverlay from "./BCRADashboardOverlay"; 
 
 export default function FloatingActions() {
   return (
     <div className="fixed left-2 bottom-24 sm:bottom-28 flex flex-col gap-2 z-50">
-      <BCRADashboardOverlay />
       <DashboardStatsV2 />
       <CVATSUploader />
       <ROICalculator />
