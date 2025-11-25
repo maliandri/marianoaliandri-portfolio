cc7b03eb3f08c4a770cc207c91dc78e21889208b
maliandri <maliandri@outlook.com>
2025-11-25 12:14:09 -0300

    especialized

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
 
 const SEO = ({ title, description, canonical, robots }) => {
   const siteUrl = 'https://www.marianoaliandri.com.ar';
   const canonicalUrl = canonical ? `${siteUrl}${canonical}` : siteUrl;
 
   return (
-    <Helmet>
+    <>
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={canonicalUrl} />
       {robots && <meta name="robots" content={robots} />}
-    </Helmet>
+    </>
   );
 };
 
