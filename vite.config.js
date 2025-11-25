65b8fd172428ebe23db48f833f779d76b337eade
maliandri <maliandri@outlook.com>
2025-11-25 00:55:25 -0300

    nuevo build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import prerender from 'vite-plugin-prerender';
 
 export default defineConfig({
   plugins: [
     react(),
-    prerender({
-      // The staticDir is no longer needed. It defaults to the correct 'dist' folder.
-      routes: ['/'],
-      rendererOptions: {
-        renderAfterElementExists: '#root',
-      }
-    }),
   ],
   server: {
     proxy: {
